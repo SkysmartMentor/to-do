a0f745f5f8011b249d1920bdbe5910ccaf20b913
SkysmartMentor <109805439+SkysmartMentor@users.noreply.github.com>
2023-02-08 18:52:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const taskInput = document.querySelector('#task');
+const taskInput = document.querySelector('.form-task');
 const addBtn = document.querySelector('#add');
 const taskList = document.querySelector('#task-list');
 
@@ -9,9 +9,17 @@ addBtn.addEventListener('click', (event) => {
 
     if (!task) return;
 
-    const li = document.createElement('li');
-    li.textContent = task;
-    taskList.appendChild(li);
+    const newItem = document.querySelector('#template').cloneNode(true).content;
+
+    newItem.querySelector('.item__text').value = task;
+    newItem.querySelector('.item__delete').addEventListener('click', (event) => {
+        event.preventDefault();
+        const target = event.target.parentElement;
+        target.remove();
+        
+    });
+
+    taskList.append(newItem);
 
     taskInput.value = '';
 });
